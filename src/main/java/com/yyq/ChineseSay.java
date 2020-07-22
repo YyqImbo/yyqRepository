77a63cf4fe0c84ed51086c6678e14104162a2243
root <yunqiang.yu@hand-china.com>
2020-07-22 22:36:25 +0800

    spring作业

---
+++ src/main/java/com/yyq/ChineseSay.java
@@ -0,0 +1,7 @@
+package com.yyq;
+
+public class ChineseSay implements ISay {
+    public void say() {
+        System.out.println("你好");
+    }
+}
---
+++ src/main/java/com/yyq/ISay.java
@@ -0,0 +1,5 @@
+package com.yyq;
+
+public interface ISay {
+    public void say();
+}
